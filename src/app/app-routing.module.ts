017f6db4f7712902fa9e54d514e7242c3837859a
Gerlinde <gerlinde@sandmair.com>
2022-06-14 09:07:10 +0200

    change line33 in app.component.spec.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent} from './home/home.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { ListComponent} from  './list/list.component';
+
+
+const routes: Routes = [
+
+  {
+
+          path:"",component: HomeComponent
+
+  },
+
+  {
+
+          path: "list",component: ListComponent
+
+  }
+
+  ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgModule } from '@angular/core';
+
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { HomeComponent } from './home/home.component';
+
+import { ListComponent } from './list/list.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+ declarations: [
+
+   AppComponent,
+
+   NavbarComponent,
+
+   HomeComponent,
+
+   ListComponent
+
+ ],
+
+ imports: [
+
+   BrowserModule,
+
+   AppRoutingModule,
+
+   ReactiveFormsModule
+
+ ],
+
+ providers: [],
+
+ bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
